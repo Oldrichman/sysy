c14a30ff8d504ecf824aad58513414836231835c
thyrien <mikko.hallberg@myy.haaga-helia.fi>
2016-02-29 15:01:25 +0200

    oikeesti kaverit

--- sysy/src/servlet/Kontrolleri.java
+++ sysy/src/servlet/Kontrolleri.java
@@ -6,7 +6,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+/*
+ * Tässä taas lisää paskaa kirjoitettuna ;D
+ */
 
 //LISÄÄ KOMMENTTIA SAATANA
 //KOMMENTTIATESTI
