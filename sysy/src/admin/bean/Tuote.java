69c097afd58a51bbde8a692cf0cc037fab3e6ea4
Hallberg Mikko <a1500910@myy.haaga-helia.fi>
2016-03-02 15:27:15 +0200

    palautus

---
+++ sysy/src/admin/bean/Tuote.java
@@ -0,0 +1,45 @@
+package admin.bean;
+
+public class Tuote {
+	
+	private int id;
+	private String nimi;
+	private double hinta;
+	
+	public Tuote() {
+		id = 0;
+		nimi = null;
+		hinta = 0;
+	}
+	
+	public Tuote(int id, String nimi, double hinta) {
+		this.id = id;
+		this.nimi = nimi;
+		this.hinta = hinta;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNimi() {
+		return nimi;
+	}
+
+	public void setNimi(String nimi) {
+		this.nimi = nimi;
+	}
+
+	public double getHinta() {
+		return hinta;
+	}
+
+	public void setHinta(double hinta) {
+		this.hinta = hinta;
+	}
+	
+}
