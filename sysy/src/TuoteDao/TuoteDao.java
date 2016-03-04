64ea242b79ec8bdaef44bbdecade4c7537d4de25
Hapuli Oskari <a1500867@myy.haaga-helia.fi>
2016-03-04 12:24:15 +0200

    More Dao

--- sysy/src/TuoteDao/TuoteDao.java
+++ sysy/src/TuoteDao/TuoteDao.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+
 import admin.bean.Tuote;
 
 public class TuoteDao {
@@ -14,8 +15,37 @@ public class TuoteDao {
 	private Connection yhteys = null;
 	
 	public void AvaaYhteys() {
+		// TIETOKANTAHAKU
 	
-	
+		String username = "";
+		String password = "";
+		String url = "";
+		
+		// YHTEYDEN AVAUS JA HAKU
+				// ajurin lataus
+	try {
+		Class.forName("org.mariadb.jdbc.Driver").newInstance();
+	} catch (Exception e1) {
+		// TODO Auto-generated catch block
+		e1.printStackTrace();
 	}
+	
+				// avataan yhteys
+	try {
+		yhteys = DriverManager.getConnection(url, username, password);
+	} catch (SQLException e) {
+		e.printStackTrace();
+		}
+	}	
+				// suljetaan yhteys
+	public void suljeYhteys() {
+		try {
+			yhteys.close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
 
 }
