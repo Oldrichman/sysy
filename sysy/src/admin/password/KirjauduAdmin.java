b3ea317f8fb7ffc42d789138ecdee9c81d17bea1
Hallberg Mikko <a1500910@myy.haaga-helia.fi>
2016-03-04 15:20:28 +0200

    adminkirjausdaopaksaska

--- sysy/src/admin/password/KirjauduAdmin.java
+++ sysy/src/admin/password/KirjauduAdmin.java
@@ -1,6 +1,11 @@
 package admin.password;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class KirjauduAdmin {
 
@@ -27,4 +32,61 @@ public class KirjauduAdmin {
 			System.out.println("V‰‰r‰ k‰ytt‰j‰tunnus");
 		}
 	}
+}
+
+protected void doPost(HttpServletRequest request,
+		HttpServletResponse response) throws ServletException, IOException {
+	response.setContentType("text/html");
+
+	response.getWriter();
+
+	// Luetaan HTML-Lomakkeelle t‰ytetyt tiedot
+
+	if (request.getParameter("nimi") != null && request.getParameter("hinta1") != null) {
+
+		// K‰yt‰ jos POISTO nappulaa ei haluta
+		// String Nimi = request.getParameter("nimi");
+		String Hinta = request.getParameter("hinta");
+		double Hinta1 = Double.parseDouble(Hinta);
+		;
+
+		System.out.println("<p>");
+		System.out.println("<b>" + pizza.getNimi() + "</b>");
+		System.out.println("<br/>");
+		System.out.println("Hinta: " + formaatteri.format(pizza.getHinta())
+				+ " EUR");
+		System.out.println("</p>");
+
+		AdminKirjausDAO akDao = new AdminKirjausDAO();
+
+		try {
+			akDao.avaaYhteys();
+			akDao.lisaaPizza(p);
+			akDao.suljeYhteys();
+		} catch (Exception e) {
+			throw new ServletException(e);
+
+		}
+	}
+
+	else {
+		AdminKirjausDAO akDao = new AdminKirjausDAO();
+
+		System.out.println("ID: " + poistettavaid);
+
+		try {
+			akDao.avaaYhteys();
+			akDao.poistaPizza(poistettavaid);
+			akDao.suljeYhteys();
+		} catch (Exception e) {
+			throw new ServletException(e);
+
+		}
+
+		response.sendRedirect("controller");
+
+	
+
+	}
+}
 }
