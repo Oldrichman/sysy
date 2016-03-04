a63a7cfaf6a99105178d9a14c52e6c5e745deb52
Hapuli Oskari <a1500867@myy.haaga-helia.fi>
2016-03-04 12:16:18 +0200

    Daota

---
+++ sysy/src/TuoteDao/TuoteDao.java
@@ -0,0 +1,21 @@
+package TuoteDao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import admin.bean.Tuote;
+
+public class TuoteDao {
+
+	private Connection yhteys = null;
+	
+	public void AvaaYhteys() {
+	
+	
+	}
+
+}
