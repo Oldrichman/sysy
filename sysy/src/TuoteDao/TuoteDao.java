efeeb51885b40907af39a3a1c9b08edf42cc580a
Hapuli Oskari <a1500867@myy.haaga-helia.fi>
2016-03-04 13:11:26 +0200

    Kontrolleri ja Dao muokattu

--- sysy/src/TuoteDao/TuoteDao.java
+++ sysy/src/TuoteDao/TuoteDao.java
@@ -46,5 +46,65 @@ public class TuoteDao {
 		}
 	}
 	
+	public ArrayList<Tuote> haeTuotteet() {
 
+		ArrayList<Tuote> tuotteet = new ArrayList<Tuote>();
+
+
+		try {
+
+			// suoritetaan haku
+			String sql = "select * from tuote";
+			Statement haku = yhteys.createStatement();
+			ResultSet resultset = haku.executeQuery(sql);
+
+			// käydään hakutulokset läpi
+			while (resultset.next()) {
+				Tuote tuote = new Tuote();
+				tuote.setId(resultset.getInt("id"));
+				tuote.setNimi(resultset.getString("nimi"));
+				tuote.setHinta(resultset.getDouble("hinta"));
+
+				/*
+				 * //lisätään tuote listaan Tuote t = new Tuote(id, nimi,
+				 * hinta); pizza.add(p); }
+				 */
+
+			}
+		} catch (Exception e) {
+			// JOTAIN VIRHEITÄ TAPAHTUI
+			System.out.println("Tietokantahaku aiheutti virheen");
+		} finally {
+			
+		}
+		
+		System.out.println("HAETTIIN TIETOKANNASTA TUOTTEET: "
+				+ tuotteet.toString());
+
+		return tuotteet;	
+	}	
+	
+	public void lisaaTuote(Tuote t) throws SQLException{
+		
+		try {
+			
+			//suoritetaan haku
+			
+			//alustetaan sql-lause
+			String sql = "insert into tuote(nimi, hinta) values(?,?)";
+			PreparedStatement resultset = yhteys.prepareStatement(sql);
+			
+			//täytetään puuttuvat tiedot
+			resultset.setString(1, t.getNimi());
+			resultset.setDouble(2, t.getHinta());
+			
+			//suoritetaan lause
+			resultset.executeUpdate();
+			System.out.println("LISÄTTIIN TUOTE TIETOKANTAAN: "+t);
+		} catch(Exception e) {
+			//JOTAIN VIRHETTÄ TAPAHTUI
+			System.out.println("Tuotteen lisäämisyritys aiheutti virheen");
+		}finally {
+		}
+	}
 }
